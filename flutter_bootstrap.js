dc0631fdce3e15f991107b9d1e1bebf4dde1edc5
kenan <kenanqanas500@gmail.com>
2024-11-11 23:32:12 +0300

    released alpha version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "513043127"
+    serviceWorkerVersion: "3763035824"
   }
 });
